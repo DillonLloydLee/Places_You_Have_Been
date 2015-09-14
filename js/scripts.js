149c9e797dae8d554cacfd1657ddcdad2c70120e
Dillon Lee & Cory Spitzer <student@epicodus.com>
2015-09-14 12:15:00 -0700

    Show hidden location details

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
     var newLocation = { name: $("input#place-name").val(),
       location: $("input#location").val(), notes: $("input#notes").val() };
 
-    $("ul.locations").append("<li><span class='location'>" + newLocation.name + "</span></li>");
+    $("ul.locations").append("<li><span class='location'>" + newLocation.name + "</span></li>" +
+                              "<div class='details'>" +
+                                "<br>" +
+                                "<p> location: " + newLocation.location + "</p>" +
+                                "<p> notes: " + newLocation.notes + "</p>" +
+                              "</div>" );
+    $(".location").click(function() {
+      console.log('clicked');
+      $(".details").css('visibility', 'visible');
+    });
   });
 });
